4165379e9505f6dffe8805f34bc75730045d1553
Olga <feskovaoa@gmail.com>
2024-03-20 16:51:56 +0300

    HW4

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -26,7 +26,7 @@ public class JpaMealRepository implements MealRepository {
             em.persist(meal);
             return meal;
         } else {
-            return em.merge(meal);
+            return get(meal.id(), userId) == null ? null: em.merge(meal);
         }
     }
 
