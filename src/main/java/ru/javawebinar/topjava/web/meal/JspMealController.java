ce1c28b6361bc302ee302877f8a0849bd386b7c8
Olga <feskovaoa@gmail.com>
2024-04-22 11:00:39 +0300

    HW6: meals controller

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -2,23 +2,70 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.javawebinar.topjava.service.MealService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.topjava.model.Meal;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
 
 @Controller
-public class JspMealController {
+@RequestMapping("/meals")
+public class JspMealController extends AbstractMealController {
     private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
 
-    @Autowired
-    private MealService mealService;
+    public Integer getId(HttpServletRequest request) {
+        String id = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(id);
+    }
 
-    @GetMapping("/meals")
-    public static String getMeals(HttpServletRequest request)
-    {
+    @GetMapping("/delete")
+    public String delete(HttpServletRequest request) {
+        super.delete(getId(request));
+        return "redirect:/meals";
+    }
+
+    @GetMapping("/update")
+    public String update(HttpServletRequest request, Model model){
+        model.addAttribute("meal", super.get(getId(request)));
+        return "mealForm";
+    }
+
+    @GetMapping("/create")
+    public String create(HttpServletRequest request, Model model){
+        model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 100));
+        return "mealForm";
+    }
+
+    @GetMapping("/filter")
+    public String filter(HttpServletRequest request, Model model){
+        LocalDate startDate = LocalDate.parse(request.getParameter("startDate"));
+        LocalDate endDate = LocalDate.parse(request.getParameter("endDate"));
+        LocalTime startTime = LocalTime.parse(request.getParameter("startTime"));
+        LocalTime endTime = LocalTime.parse(request.getParameter("endTime"));
+        model.addAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
         return "meals";
+    }
+
+    @PostMapping()
+    public String updateOrCreate(HttpServletRequest request){
+        Meal meal = new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        if(request.getParameter("id").isEmpty()){
+            super.create(meal);
+        }
+        else {
+            super.update(meal, getId(request));
+        }
+        return "redirect:/meals";
     }
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -9,8 +9,7 @@
 <jsp:include page="fragments/bodyHeader.jsp"/>
 <section>
     <h2><spring:message code="meal.title"/></h2>
-    <form method="get" action="meals">
-        <input type="hidden" name="action" value="filter">
+    <form method="get" action="meals/filter">
         <dl>
             <dt><spring:message code="meal.from_date"/></dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
@@ -30,7 +29,7 @@
         <button type="submit"><spring:message code="meal.filter"/></button>
     </form>
     <hr/>
-    <a href="meals?action=create"><spring:message code="meal.add_meal"/></a>
+    <a href="meals/create"><spring:message code="meal.add_meal"/></a>
     <br><br>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -53,8 +52,8 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals?action=update&id=${meal.id}">common.update</a></td>
-                <td><a href="meals?action=delete&id=${meal.id}">common.delete</a></td>
+                <td><a href="meals/update?id=${meal.id}"><spring:message code="common.update"/></a></td>
+                <td><a href="meals/delete?id=${meal.id}"><spring:message code="common.delete"/></a></td>
             </tr>
         </c:forEach>
     </table>
